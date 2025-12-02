4fa8938240e77b7260e2ae8d72de8c82f23a9b21
Kaushal55555 <lordqshal@gmail.com>
2025-12-02 11:02:50 +0545

    Add project files.

---
+++ week6Tasks/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("===== TASK 1: Expression Bodied Members =====");
+        Rectangle rect = new Rectangle(10, 5);
+        Console.WriteLine($"Area: {rect.Area}");
+        Console.WriteLine($"Perimeter: {rect.Perimeter}");
+        Console.WriteLine();
+    }
+}
---
+++ week6Tasks/Task1/Rectangle.cs
@@ -0,0 +1,12 @@
+﻿public class Rectangle
+{
+    public double Length { get; }
+    public double Width { get; }
+
+    public Rectangle(double length, double width)
+        => (Length, Width) = (length, width);
+
+    public double Area => Length * Width;
+
+    public double Perimeter => 2 * (Length + Width);
+}
